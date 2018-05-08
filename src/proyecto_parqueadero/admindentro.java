eaef069c8b78f7f53448fe1cf161855aa22d3c9a
xxxREIDENxxx <reiden-3@hotmail.com>
2018-05-08 16:14:49 -0500

    Tabla dever clientes plus

--- src/proyecto_parqueadero/admindentro.java
+++ src/proyecto_parqueadero/admindentro.java
@@ -121,7 +121,7 @@ public class admindentro extends javax.swing.JFrame {
     }//GEN-LAST:event_bn_verclientesActionPerformed
 
     private void bn_verclientesplusActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bn_verclientesplusActionPerformed
-       
+        ver_cliente_plus verCP = new ver_cliente_plus(registro);
     }//GEN-LAST:event_bn_verclientesplusActionPerformed
 
     private void bn_cienteplusActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bn_cienteplusActionPerformed
